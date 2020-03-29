dc6a3a8cca5289e1f5631db01f409088fafe915d
Vahan Mkrtchyan <vahan.mkrtchyan@atlas-iac.com>
2020-03-30 00:30:46 +0300

    pointers

--- wtf.go
+++ wtf.go
@@ -35,20 +35,20 @@ func (e Failure) Error() string {
 	return e.MessageString()
 }
 
-func (e *Failure) WithMessage(message string) Failure {
+func (e *Failure) WithMessage(message string) *Failure {
 	e.ExtraMessage = message
-	return *e
+	return e
 }
 
-func (e *Failure) WitCode(code int) Failure {
+func (e *Failure) WitCode(code int) *Failure {
 	e.Code = code
-	return *e
+	return e
 }
 
-func Wrap(err interface{}) Failure {
+func Wrap(err interface{}) *Failure {
 	switch err.(type) {
 	case Failure:
-		return err.(Failure)
+		return err.(*Failure)
 	case error:
 		return New(err.(error).Error(), mainConfig.DefaultErrorCode)
 	case string:
@@ -62,11 +62,11 @@ func (e Failure) Panic() {
 	panic(e)
 }
 
-func New(message string, code int) Failure {
-	return Failure{Code: code, Message: message}
+func New(message string, code int) *Failure {
+	return &Failure{Code: code, Message: message}
 }
 
-func Define(message string, code int) Failure {
+func Define(message string, code int) *Failure {
 	return New(message, code)
 }
 
@@ -78,6 +78,6 @@ func IsFailure(toTest error) bool {
 	return false
 }
 
-func Default() Failure {
+func Default() *Failure {
 	return New(mainConfig.WrapUnknownMessage, mainConfig.DefaultErrorCode)
 }
